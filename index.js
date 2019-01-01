ab7d7e0f7ae30c5b999c7f4b8d31943095e027a7
CREGATHERTIVE <45475847+CREGATHERTIVE@users.noreply.github.com>
2019-01-01 21:57:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ exports.reply = function justReply(req, res) {
 
         var imgur_options = {
             method: 'GET',
-            uri: `https://api.imgur.com/3/album/${target_albumId}/`,
+            uri: `https://api.imgur.com/3/album/${target_albumId}/images`,
             headers: {
               "Authorization": `Client-ID ${193e2d7faeaa986}`
             },
